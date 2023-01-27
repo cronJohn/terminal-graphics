546ce1e2f7a4215c6d0f569e174cccea33582541
John Niemiec <51012201+cronJohn@users.noreply.github.com>
2023-01-26 18:25:15 -0600

    Added main script file and updated README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # terminal-graphics
 Let's you display an image or video graphic while your cli program is loading
+
+# How
+- Prefix whatever command you want to run with this program (main.sh)
+
+## Syntax
+### Just the command
+`main.sh <command>`
+
+### With options and command
+`main.sh <options>,<command>`
+
+---
+# Issues
+- The gif keeps playing even when the command ends
+
+---
+# Credit
+- Uses Google's 'gif-for-cli' program: https://github.com/google/gif-for-cli
---
+++ main.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+install_deps_if_not_installed() {
+    if ! command -v gif-for-cli > /dev/null; then
+        echo "Installing dependencies..."
+
+        git clone "https://github.com/google/gif-for-cli" .
+        cd 'gif-for-cli'
+
+        # Fix an issue with failing to import the 'third_party' module
+        rm gif_for_cli/third_party
+        cp third_party gif_for_cli/third_party  -r
+
+        python3 setup.py install --user
+
+        # Add to path
+        if [ -d "$HOME/.local/bin" ] ; then
+            PATH="$HOME/.local/bin:$PATH"
+        fi
+
+        # Check if installation worked
+        if command -v gif-for-cli > /dev/null; then
+            echo "Installation succeeded."
+        else
+            echo "Couldn't install gif-for-cli"
+            sleep 2
+            echo "Check the docs at https://github.com/google/gif-for-cli for more information."
+            exit 1
+        fi
+    fi
+}
+
+draw_ascii(){
+    eval "$(which gif-for-cli) "$options" "--cols $(tput cols)" "--rows $(tput lines)" -l 1 'party parrot'"
+}
+
+run_cmd(){
+    $cmd > /dev/null &
+}
+
+examples_dir='examples'
+
+# main.sh options,cmd
+user_input="$*"
+IFS=',' read -r -a parts <<< "$user_input"
+
+# check if the user didn't input options
+if [ -z "${parts[1]}" ] ; then
+    cmd="${parts[0]}"
+else
+    options="${parts[0]}"
+    cmd="${parts[1]}"
+fi
+
+IFS=' '
+
+
+# Main execution
+install_deps_if_not_installed
+run_cmd
+draw_ascii
+
+
